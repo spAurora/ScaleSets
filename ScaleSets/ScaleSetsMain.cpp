4286a9266edf8e38120f2c20509a599be048c5f2
HaoyuWang <751984964@qq.com>
2019-10-17 13:33:09 +0800

    配置好了环境

---
+++ ScaleSets/ScaleSetsMain.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <list>
+#include <vector>
+#include <cstdlib>
+#include <forward_list>
+#include <cmath>
+#include <time.h>
+#include "highgui.h"
+#include "cv.h"
+
+using namespace cv;
+
+int main()
+{
+	
+
+	return 0;
+}
